4f643993e0d28e31a6d39a8e4f4e1eb2c54e6c7c
harris9 <harris@idu-identification.com>
2022-07-18 23:24:35 +1000

    system color theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import { primaryTheme } from '~/providers/chakra';
 
 export default class _Document extends NextDocument {
   render() {
@@ -9,9 +8,7 @@ export default class _Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
-          <ColorModeScript
-            initialColorMode={primaryTheme.config.initialColorMode}
-          />
+          <ColorModeScript initialColorMode="system" />
           <Main />
           <NextScript />
         </body>
