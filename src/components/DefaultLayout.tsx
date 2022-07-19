04461ff84841293796b6097d3c69c3563cce9c94
harris9 <harris@idu-identification.com>
2022-07-19 19:49:11 +1000

    Removed sidebar code

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,64 +1,26 @@
-import { SettingsIcon } from '@chakra-ui/icons';
-import { Box, Flex, Icon, useDisclosure } from '@chakra-ui/react';
-import { Role } from '@prisma/client';
-import { signIn, signOut, useSession } from 'next-auth/react';
+/**
+ *
+ * Default layout
+ * Wraps other pages
+ *
+ */
+import { Box, Flex } from '@chakra-ui/react';
 import Head from 'next/head';
-import React, { ReactNode, useMemo } from 'react';
-import { MdAdminPanelSettings, MdLogin, MdLogout } from 'react-icons/md';
+import React, { ReactNode } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Footer from '~/components/Footer';
 import Header from '~/components/Header';
-import Sidebar, { ISidebarContent } from '~/components/Sidebar';
 
 type DefaultLayoutProps = { children: ReactNode };
 
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
-  const sidebarControls = useDisclosure();
-
-  const { data: session } = useSession();
-
-  const SidebarHeader: ISidebarContent[] = useMemo(() => [], []);
-
-  const SidebarContent: ISidebarContent[] = useMemo(() => [], []);
-
-  const SidebarFooter: ISidebarContent[] = useMemo(
-    () => [
-      {
-        icon: <Icon as={MdAdminPanelSettings} />,
-        title: 'admin',
-        href: '/admin',
-        hidden: !session || session?.user?.role !== Role.ADMIN,
-      },
-      {
-        icon: <Icon as={SettingsIcon} />,
-        title: 'settings',
-        href: '/settings',
-        disabled: !session,
-      },
-      {
-        icon: <Icon as={session ? MdLogout : MdLogin} />,
-        onClick: session ? () => signOut({ callbackUrl: '/' }) : () => signIn(),
-        title: session ? 'logout' : 'login',
-        href: '',
-      },
-    ],
-    [session],
-  );
-
   return (
     <>
       <Head>
-        <title>Prisma Starter</title>
+        <title>Elixir</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* Sidebar placement doesnt matter as its a drawer */}
-      <Sidebar
-        sidebarControls={sidebarControls}
-        headerContent={SidebarHeader}
-        bodyContent={SidebarContent}
-        footerContent={SidebarFooter}
-      />
-      <Header sidebarControls={sidebarControls} />
+      <Header />
       <Flex direction="column" flex={1} minHeight="100vh" maxWidth="100vw">
         <Box flex={1} padding="16px">
           {children}
